206aa00488e7e801ab7db5224ee3cb61dcd2499d
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2026-01-06 21:49:27 +0100

    solved number 1 question

---
+++ arrays-objects-assignment.js
@@ -0,0 +1,6 @@
+const students = [
+    {id: 1, name: "Kassie", age: 27, grades:[75, 80, 100]}, 
+    {id: 2, name: "Chisom", age: 25, grades:[65, 40, 89]},
+    {id: 3, name: "Ugochi", age: 23, grades:[57, 75, 94]},
+    {id: 4, name: "Ifunaya", age: 21, grades:[50, 92, 87]}
+]
