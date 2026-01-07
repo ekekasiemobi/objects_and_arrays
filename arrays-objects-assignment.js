212414ec8a7ce8708a4640daac53b866dd78cbce
ekekasiemobi <kasiemobijanfrancis@gmail.com>
2026-01-07 12:29:02 +0100

    calculate average grade

--- arrays-objects-assignment.js
+++ arrays-objects-assignment.js
@@ -1,6 +1,19 @@
 const students = [
     {id: 1, name: "Kassie", age: 27, grades:[75, 80, 100]}, 
-    {id: 2, name: "Chisom", age: 25, grades:[65, 40, 89]},
+    {id: 2, name: "Chisom", age: 25, grades:[65, 40, 19]},
     {id: 3, name: "Ugochi", age: 23, grades:[57, 75, 94]},
-    {id: 4, name: "Ifunaya", age: 21, grades:[50, 92, 87]}
+    {id: 4, name: "Ifunaya", age: 21, grades:[25, 72, 80]}
 ]
+ 
+function calculateAverage(grades){
+  const average = grades.reduce((sum, x) => sum + x, 0)/ grades.length
+  return Number(average.toFixed(2))
+}
+
+const studentsWithAverage = students.map(student => {
+  return{
+  ...student, average: calculateAverage(student.grades)}
+})
+
+console.log(studentsWithAverage)
+
